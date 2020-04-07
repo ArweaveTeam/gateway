820822fcda574e97fc3b9c5af076105c1d75aeab
Kyle Beckles <kyle@arweave.org>
2020-04-07 23:04:24 +0200

    Added postgres upsert

--- src/lib/buckets.ts
+++ src/lib/buckets.ts
@@ -2,14 +2,14 @@ import { S3 } from "aws-sdk";
 import { toB64url, Base64UrlEncodedString } from "./encoding";
 
 const buckets: { [key in BucketType]: string } = {
-  "tx-data": process.env.ARWEAVE_S3_TX_DATA_BUCKET!
+  "tx-data": process.env.ARWEAVE_S3_TX_DATA_BUCKET!,
 };
 
 type BucketType = "tx-data";
 
 export type BucketObject = S3.GetObjectOutput;
 
-const s3 = new S3();
+const s3 = new S3({ httpOptions: { timeout: 5000, connectTimeout: 5000 } });
 
 export const put = async (
   bucketType: BucketType,
@@ -26,7 +26,7 @@ export const put = async (
       Key: key,
       Bucket: bucket,
       Body: body,
-      ContentType: contentType
+      ContentType: contentType,
     })
     .promise();
 };
@@ -35,7 +35,7 @@ export const getEncoded = async (
   bucketType: BucketType,
   key: string
 ): Promise<Base64UrlEncodedString> => {
-  return get(bucketType, key).then(bucketObject =>
+  return get(bucketType, key).then((bucketObject) =>
     toB64url(bucketObject.Body as Buffer)
   );
 };
@@ -49,7 +49,7 @@ export const get = async (
   return s3
     .getObject({
       Key: key,
-      Bucket: bucket
+      Bucket: bucket,
     })
     .promise();
 };
--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -1,12 +1,12 @@
 import { RDS } from "aws-sdk";
-import knex from "knex";
+import knex, { Transaction } from "knex";
 
 type ConnectionMode = "read" | "write";
 
 export const createConnection = (mode: ConnectionMode): knex => {
   const host = {
     read: process.env.ARWEAVE_DB_READ_HOST,
-    write: process.env.ARWEAVE_DB_WRITE_HOST
+    write: process.env.ARWEAVE_DB_WRITE_HOST,
   }[mode];
 
   console.log("Connecting to db", mode, host, process.env.ARWEAVE_DB_SCHEMA);
@@ -20,18 +20,49 @@ export const createConnection = (mode: ConnectionMode): knex => {
         user: mode,
         database: process.env.ARWEAVE_DB_SCHEMA,
         ssl: {
-          rejectUnauthorized: true
+          rejectUnauthorized: false,
         },
         password: new RDS.Signer().getAuthToken({
           region: process.env.AWS_REGION,
           hostname: host,
           port: 5432,
-          username: mode
+          username: mode,
         }),
-        expirationChecker: () => true
+        expirationChecker: () => true,
       };
-    }
+    },
   });
 
   return client;
+};
+
+interface UpsertOptions {
+  table: string;
+  conflictKeys: string[];
+  rows: { [key: string]: any }[];
+}
+
+/**
+ * Generate a postgres upsert statement. This manually appends a raw section to the query.
+ *
+ * INSERT (col, col, col) VALUES (val, val, val) ON CONFLICT (id,index) SO UPDATE SET x = excluded.x...
+ */
+export const upsert = (
+  knexTransaction: knex.Transaction,
+  { table, conflictKeys, rows }: UpsertOptions
+) => {
+  const updateFields = Object.keys(rows[0])
+    .filter((field) => !conflictKeys.includes(field))
+    .map((field) => `${field} = excluded.${field}`)
+    .join(",");
+
+  const { sql, bindings } = knexTransaction.into(table).insert(rows).toSQL();
+
+  const upsertSql = sql.concat(
+    ` ON CONFLICT (${conflictKeys.join(",")}) DO UPDATE SET ${updateFields};`
+  );
+
+  console.log("Query", upsertSql, bindings);
+
+  return knexTransaction.raw(upsertSql, bindings);
 };
