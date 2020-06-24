0481b61f04bed15dc31928afd002c2b954cb3540
Kyle Beckles <kyle@arweave.org>
2020-06-24 12:57:28 +0200

    Added basic chunk proof validation

--- src/gateway/routes/new-chunk/index.ts
+++ src/gateway/routes/new-chunk/index.ts
@@ -5,16 +5,33 @@ import { pick } from "lodash";
 import { ImportChunk } from "../../../interfaces/messages";
 import { RequestHandler } from "express";
 import { put } from "../../../lib/buckets";
+import NodeCryptoDriver from "arweave/node/lib/crypto/node-driver";
+import Arweave from "arweave/node";
+
+Arweave.crypto = new NodeCryptoDriver();
+
+import { validatePath } from "arweave/node/lib/merkle";
+import { BadRequest } from "http-errors";
 
 export const handler: RequestHandler = async (req, res, next) => {
   const chunk: Chunk = req.body;
-
-  const chunkData = fromB64Url(chunk.chunk);
 
   req.log.info(`[new-chunk] received new chunk`, {
     ...chunk,
     chunk: chunk.chunk && chunk.chunk.substr(0, 100) + "...",
   });
+
+  const chunkData = parseOrThrow(chunk.chunk, "chunk");
+
+  const dataPath = parseOrThrow(chunk.data_path, "data_path");
+
+  const root = parseOrThrow(chunk.data_root, "data_root");
+
+  const isValid = validateChunk(root, chunk.offset, chunk.data_size, dataPath);
+
+  if (!isValid) {
+    throw new BadRequest("validation_failed");
+  }
 
   await put("tx-data", `chunks/${chunk.data_root}/${chunk.offset}`, chunkData, {
     contentType: "application/octet-stream",
@@ -26,4 +43,25 @@ export const handler: RequestHandler = async (req, res, next) => {
   });
 
   res.sendStatus(200);
+};
+
+const parseOrThrow = (b64urlString: string, fieldName: string) => {
+  try {
+    return fromB64Url(b64urlString);
+  } catch (error) {
+    throw new BadRequest(`missing field: ${fieldName}`);
+  }
+};
+
+const validateChunk = async (
+  root: Buffer,
+  offset: number,
+  size: number,
+  proof: Buffer
+) => {
+  try {
+    return await validatePath(root, offset, 0, size, proof);
+  } catch (error) {
+    throw new BadRequest("validation_failed");
+  }
 };
