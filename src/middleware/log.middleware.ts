c31ad6d39b473d68c7b04767120b2547cd007137
Al <al@openkoi.com>
2021-02-07 18:27:56 -0400

    moving logs to the root of src/. Run and check /dist/src/ for the log file. Test with

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 import path from 'path';
 
 // TODO: @theloneronin what's the best place to put this file?
-var logFileLocation = path.join(__dirname, 'access.log')
+var logFileLocation = path.join(__dirname, '../../access.log')
 var accessLogStream = fs.createWriteStream(logFileLocation, { flags: 'a' })
 
 export function logConfigurationMiddleware(req: Request, res: Response, next: NextFunction) {
@@ -21,4 +21,5 @@ morgan.token(`trace`, (req: Request) => {
   return req.id || `UNKNOWN`;
 });
 
+// TODO - add encryption on the line below for :remote-addr to protect viewer's privacy
 export const logMiddleware = morgan('[http] :remote-addr - :remote-user [:date] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent" [trace=:trace]', { stream: accessLogStream});
