fc05a3d91eb0edf80423a394c52c71d9a72a6f0d
Al <al@openkoi.com>
2021-02-07 17:47:06 -0400

    adding write to log file -> access.log

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -1,5 +1,7 @@
 import morgan from 'morgan';
 import id from 'shortid';
+import fs from 'fs';
+import path from 'path';
 import {Request, Response, NextFunction} from 'express';
 
 export function logConfigurationMiddleware(req: Request, res: Response, next: NextFunction) {
@@ -11,8 +13,10 @@ export function logConfigurationMiddleware(req: Request, res: Response, next: Ne
   return next();
 }
 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+ 
 morgan.token(`trace`, (req: Request) => {
   return req.id || `UNKNOWN`;
 });
 
-export const logMiddleware = morgan('[http] :remote-addr - :remote-user [:date] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent" [trace=:trace]');
+export const logMiddleware = morgan('[http] :remote-addr - :remote-user [:date] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent" [trace=:trace]', { stream: accessLogStream });
