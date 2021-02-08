ad1c347bf08ee61d62e90803625d93f4e76b73ab
The Lone Ronin <log2n@protonmail.com>
2021-02-08 17:49:10 -0500

    Improve and refactor proxy router

--- src/route/transaction.route.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response} from 'express';
-
-export async function transactionOptionsRoute(req: Request, res: Response) {
-  return res.send('OK').end();
-}
-
-export async function transactionRoute(req: Request, res: Response) {
-
-}
