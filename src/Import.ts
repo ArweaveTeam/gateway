a2149c6c20692fd3dc1357c37b769c2348892775
The Lone Ronin <log2n@protonmail.com>
2021-02-11 21:39:48 -0500

    Full snapshot synchronization with ANS-102 compatibility

--- src/Import.ts
+++ src/Import.ts
@@ -1,84 +1,17 @@
-import {config} from 'dotenv';
-import {connection} from './database/connection.database';
 import {log} from './utility/log.utility';
-import {transactionFields} from './database/transaction.database';
-
-config();
-
-export const indices = JSON.parse(process.env.INDICES || '[]') as Array<string>;
+import {importBlocks, importTransactions, importTags} from './database/import.database';
 
 export async function importSnapshot() {
-  await importBlocks();
-  await importTransactions();
-  await importTags();
+  await importBlocks(`${process.cwd()}/snapshot/block.csv`);
+  log.info('[snapshot] successfully imported block.csv');
+
+  await importTransactions(`${process.cwd()}/snapshot/transaction.csv`);
+  log.info('[snapshot] successfully imported transaction.csv');
+
+  await importTags(`${process.cwd()}/snapshot/tags.csv`);
+  log.info('[snapshot] successfully imported tags.csv');
 
   process.exit();
-}
-
-export async function importBlocks() {
-  return new Promise(async (resolve) => {
-    await connection.raw(`
-      COPY
-        blocks
-        (id, previous_block, mined_at, height, txs, extended)
-      FROM
-        '${process.cwd()}/snapshot/block.csv'
-      WITH
-        (
-          FORMAT CSV,
-          ESCAPE '\\',
-          DELIMITER ',',
-          FORCE_NULL(height)
-        )
-      `);
-    log.info(`[snapshot] successfully imported block.csv`);
-    return resolve(true);
-  });
-}
-
-export async function importTransactions() {
-  return new Promise(async (resolve) => {
-    const fields = transactionFields
-      .concat(indices)
-      .map(field => `"${field}"`);
-
-    await connection.raw(`
-      COPY
-        transactions
-        (${fields.join(',')})
-      FROM
-        '${process.cwd()}/snapshot/transaction.csv'
-      WITH
-        (
-          FORMAT CSV,
-          ESCAPE '\\',
-          DELIMITER ',',
-          FORCE_NULL("format", "height", "data_size")
-        )`);
-    log.info(`[snapshot] successfully imported transaction.csv`);
-    return resolve(true);
-  });
-}
-
-export async function importTags() {
-  return new Promise(async (resolve) => {
-    await connection.raw(`
-      COPY
-        tags
-        (tx_id, index, name, value)
-      FROM
-        '${process.cwd()}/snapshot/tags.csv'
-      WITH
-        (
-          FORMAT CSV,
-          ESCAPE '\\',
-          DELIMITER ',',
-          FORCE_NULL(index)
-        )
-      `);
-    log.info(`[snapshot] successfully imported tags.csv`);
-    return resolve(true);
-  });
 }
 
 (async () => await importSnapshot())();
--- src/Snapshot.ts
+++ src/Snapshot.ts
@@ -1,25 +1,28 @@
 import ProgressBar from 'progress';
+import {DataItemJson} from 'arweave-bundles';
 import {config} from 'dotenv';
 import {existsSync, readFileSync, writeFileSync, createWriteStream} from 'fs';
+import {ansBundles} from './utility/ans.utility';
 import {mkdir} from './utility/file.utility';
 import {log} from './utility/log.utility';
-import {getNodeInfo} from './query/node.query';
+import {sleep} from './utility/sleep.utility';
+import {getNodeInfo, getData} from './query/node.query';
 import {block} from './query/block.query';
-import {transaction} from './query/transaction.query';
+import {transaction, tagValue, Tag} from './query/transaction.query';
 import {formatBlock} from './database/block.database';
 import {transactionFields, DatabaseTag, formatTransaction, formatAnsTransaction} from './database/transaction.database';
 
 config();
+mkdir('snapshot');
+
+export const indices = JSON.parse(process.env.INDICES || '[]') as Array<string>;
+export const parallelization = parseInt(process.env.PARALLEL || '8');
 
 export let SIGINT: boolean = false;
 export let SIGKILL: boolean = false;
 
-export let snapshotBar: ProgressBar;
+export let bar: ProgressBar;
 export let topHeight = 0;
-
-export const indices = JSON.parse(process.env.INDICES || '[]') as Array<string>;
-
-mkdir('snapshot');
 
 export const streams = {
   block: createWriteStream('snapshot/block.csv', {flags: 'a'}),
@@ -28,7 +31,7 @@ export const streams = {
 };
 
 export function configureSnapshotBar(start: number, end: number) {
-  snapshotBar = new ProgressBar(
+  bar = new ProgressBar(
       ':current/:total blocks synced [:bar] :percent :etas',
       {
         complete: '|',
@@ -48,7 +51,8 @@ export async function snapshot() {
       configureSnapshotBar(snapshotState, nodeInfo.height);
       topHeight = nodeInfo.height;
       log.info(`[snapshot] snapshot is currently at height ${snapshotState}, resuming sync to ${topHeight}`);
-      await storeBlock(snapshotState + 1);
+      bar.tick();
+      await parallelize(snapshotState + 1);
     } else {
       log.info('[snapshot] snapshot state is malformed. Please make sure it is a number');
       process.exit();
@@ -58,7 +62,44 @@ export async function snapshot() {
     configureSnapshotBar(0, nodeInfo.height);
     topHeight = nodeInfo.height;
     log.info(`[snapshot] new snapshot is being generated, syncing from block 0 to ${topHeight}`);
-    await storeBlock(0);
+    bar.tick();
+    await parallelize(0);
+  }
+}
+
+export async function parallelize(height: number) {
+  if (height >= topHeight) {
+    log.info(`[snapshot] fully synced, monitoring for new blocks`);
+    await sleep(30000);
+    const nodeInfo = await getNodeInfo();
+    if (nodeInfo.height > topHeight) {
+      log.info(`[snapshot] updated height from ${topHeight} to ${nodeInfo.height} syncing new blocks`)
+    }
+    topHeight = nodeInfo.height;
+    await parallelize(height);
+  } else {
+    const batch = [];
+
+    for (let i = height; i < height + parallelization && i < topHeight; i++) {
+      batch.push(storeBlock(i));
+    }
+  
+    SIGINT = true;
+  
+    await Promise.all(batch);
+    
+    if (!bar.complete) {
+      bar.tick(batch.length);
+    }
+
+    writeFileSync('.snapshot', (height + batch.length).toString());
+    writeFileSync('snapshot/.snapshot', (height + batch.length).toString());
+  
+    SIGINT = false;
+  
+    if (SIGKILL === false) {
+      await parallelize(height + batch.length);
+    }  
   }
 }
 
@@ -66,27 +107,12 @@ export async function storeBlock(height: number) {
   try {
     const currentBlock = await block(height);
     const fb = formatBlock(currentBlock);
+    const input = `"${fb.id}","${fb.previous_block}","${fb.mined_at}","${fb.height}","${fb.txs.replace(/"/g, '\\"')}","${fb.extended.replace(/"/g, '\\"')}"\n`;
 
-    streams.block.write(`"${fb.id}","${fb.previous_block}","${fb.mined_at}","${fb.height}","${fb.txs.replace(/"/g, '\\"')}","${fb.extended.replace(/"/g, '\\"')}"\n`);
+    streams.block.write(input);
 
     if (height > 0) {
       await storeTransactions(JSON.parse(fb.txs) as Array<string>, height);
-    }
-
-    SIGINT = true;
-
-    snapshotBar.tick();
-    writeFileSync('.snapshot', height.toString());
-
-    if (height === topHeight) {
-      log.info('[snapshot] synced to the specified height. Rerun yarn dev:snapshot to sync to the latest height');
-      process.exit();
-    }
-
-    SIGINT = false;
-
-    if (SIGKILL === false) {
-      await storeBlock(height + 1);
     }
   } catch (error) {
     log.info(`[snapshot] could not retrieve block at height ${height}, retrying`);
@@ -111,27 +137,81 @@ export async function storeTransaction(tx: string, height: number) {
   try {
     const currentTransaction = await transaction(tx);
     const ft = formatTransaction(currentTransaction);
+    const preservedTags = JSON.parse(ft.tags) as Array<Tag>;
     ft.tags = `${ft.tags.replace(/"/g, '\\"')}`;
 
     const fields = transactionFields
         .map((field) => `"${ft[field] ? ft[field] : ''}"`)
         .concat(indices.map((ifield) => `"${ft[ifield] ? ft[ifield] : ''}"`));
 
-    streams.transaction.write(`${fields.join(',')}\n`);
+    const input = `${fields.join(',')}\n`;
 
-    storeTags(JSON.parse(ft.tags) as Array<DatabaseTag>);
+    streams.transaction.write(input);
+
+    storeTags(ft.id, preservedTags);
+
+    const ans102 = tagValue(preservedTags, 'Bundle-Type') === 'ANS-102';
+
+    if (ans102) {
+      try {
+        const ansPayload = await getData(ft.id);
+        const ansTxs = await ansBundles.unbundleData(ansPayload);
+
+        await processANSTransaction(ansTxs);
+      } catch (error) {
+        console.log('');
+        log.info(`[snapshot] malformed ANS payload at height ${height} for tx ${ft.id}`);
+      }
+    }
   } catch (error) {
+    console.log('');
     log.info(`[snapshot] could not retrieve tx ${tx} at height ${height}`);
   }
 }
 
-export function storeTags(tags: Array<DatabaseTag>) {
-  for (let i = 0; i < tags.length; i++) {
-    const tag = tags[i];
-    streams.tags.write(`"${tag.tx_id}","${tag.index}","${tag.name}","${tag.value}"\n`);
+export async function processANSTransaction(ansTxs: Array<DataItemJson>) {
+  for (let i = 0; i < ansTxs.length; i++) {
+    const ansTx = ansTxs[i];
+    const ft = formatAnsTransaction(ansTx);
+    ft.tags = `${ft.tags.replace(/"/g, '\\"')}`;
+
+    const ansTags = ansTx.tags;
+
+    const fields = transactionFields
+        .map((field) => `"${ft[field] ? ft[field] : ''}"`)
+        .concat(indices.map((ifield) => `"${ft[ifield] ? ft[ifield] : ''}"`));
+
+    const input = `${fields.join(',')}\n`;
+
+    streams.transaction.write(input);
+
+    for (let ii = 0; ii < ansTags.length; ii++) {
+      const ansTag = ansTags[ii];
+      const {name, value} = ansTag;
+
+      const tag: DatabaseTag = {
+        tx_id: ansTx.id,
+        index: ii,
+        name: name || '',
+        value: value || '',
+      };
+
+      const input = `"${tag.tx_id}","${tag.index}","${tag.name}","${tag.value}"\n`;
+
+      streams.tags.write(input);
+    }
   }
 }
 
+export function storeTags(tx_id: string, tags: Array<Tag>) {
+  for (let i = 0; i < tags.length; i++) {
+    const tag = tags[i];
+
+    const input = `"${tx_id}","${i}","${tag.name}","${tag.value}"\n`;
+
+    streams.tags.write(input);
+  }
+}
 (async () => await snapshot())();
 
 process.on('SIGINT', () => {
