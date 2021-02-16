c59157440f526ce1b00a566f970ca83b2db39455
The Lone Ronin <log2n@protonmail.com>
2021-02-16 12:21:39 -0500

    Convert transaction payloads into piped streams

--- src/route/data.route.ts
+++ src/route/data.route.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from 'express';
-import {getData} from '../query/node.query';
+import {getDataAsStream} from '../query/node.query';
 
 export const dataRouteRegex = /^\/?([a-zA-Z0-9-_]{43})\/?$|^\/?([a-zA-Z0-9-_]{43})\/(.*)$/i;
 export const pathRegex = /^\/?([a-z0-9-_]{43})/i;
@@ -7,7 +7,7 @@ export const pathRegex = /^\/?([a-z0-9-_]{43})/i;
 export async function dataRoute(req: Request, res: Response) {
   const path = req.path.match(pathRegex) || [];
   const transaction = path.length > 1 ? path[1] : '';
-  const payload = await getData(transaction);
+  const payload = getDataAsStream(transaction);
 
-  return res.status(200).send(payload);
+  return payload.pipe(res);
 }
