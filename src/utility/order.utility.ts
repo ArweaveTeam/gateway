1fa1e40069d8976f08118597ad7feb949e077fd1
The Lone Ronin <log2n@protonmail.com>
2021-04-01 13:30:55 -0400

    v0.7.2 patches

--- src/utility/order.utility.ts
+++ src/utility/order.utility.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+config();
+
 export const indices = JSON.parse(process.env.INDICES || '[]') as Array<string>;
 export const blockOrder = ['id', 'previous_block', 'mined_at', 'height', 'txs', 'extended'];
 export const transactionOrder = ['format', 'id', 'signature', 'owner', 'owner_address', 'target', 'reward', 'last_tx', 'height', 'tags', 'quantity', 'content_type', 'data_size', 'data_root'];
