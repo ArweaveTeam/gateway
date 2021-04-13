b440f871d68e6e5f0129d87841a4fa7f504c6803
Al <al@openkoi.com>
2021-04-13 12:33:01 -0300

    cleaning up type errors

--- src/Gateway.ts
+++ src/Gateway.ts
@@ -1,7 +1,6 @@
 import 'colors';
-import express, {Express} from 'express';
+import express, {Express, RequestHandler, Request, Response} from 'express';
 import {config} from 'dotenv';
-import cron from 'node-cron';
 import {corsMiddleware} from './middleware/cors.middleware';
 import {jsonMiddleware} from './middleware/json.middleware';
 import {logMiddleware} from './middleware/log.middleware';
@@ -11,23 +10,28 @@ import {statusRoute} from './route/status.route';
 import {proxyRoute} from './route/proxy.route';
 import {dataRouteRegex, dataHeadRoute, dataRoute} from './route/data.route';
 import {startSync} from './database/sync.database';
-import {logsHelper, logsTask} from './utility/log.helper';
+import KoiLogs from "koi-logs";
 
 config();
 
 export const app: Express = express();
+
+const koiLogger = new KoiLogs('./');
+
+app.get('/logs/', async function(req: Request, res: Response) {
+  return koiLogger.koiLogsHelper(req, res);
+}) as RequestHandler;
+app.get('/logs/raw/', async function(req: Request, res: Response) { 
+  return koiLogger.koiRawLogsHelper(req, res);
+}) as RequestHandler;
+
+app.use(koiLogger.logger);
 
 export function start() {
   app.set('trust proxy', 1);
   app.use(corsMiddleware);
   app.use(jsonMiddleware);
   app.use(logMiddleware);
-
-  cron.schedule('0 0 * * *', async function() {
-    console.log('running the log cleanup task once per day on ', new Date() );
-    const result = await logsTask();
-    console.log('daily log task returned ', result);
-  });
 
   app.get('/', statusRoute);
   app.head(dataRouteRegex, dataHeadRoute);
@@ -36,7 +40,6 @@ export function start() {
   graphServer({introspection: true, playground: true}).applyMiddleware({app, path: '/graphql'});
 
   app.get(dataRouteRegex, dataRoute);
-  app.get('/logs', logsHelper);
 
   app.all('*', proxyRoute);
 
