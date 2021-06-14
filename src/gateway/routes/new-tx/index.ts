d586d70a219232531c104fc457a70768ea48aed0
João Rodrigues <joaop86@hotmail.com>
2021-06-14 10:36:20 +0100

    Change from hosts to amplify gateway

--- src/gateway/routes/new-tx/index.ts
+++ src/gateway/routes/new-tx/index.ts
@@ -14,7 +14,6 @@ import { broadcastTx } from "../../../lib/broadcast";
 
 import Joi, { Schema } from "@hapi/joi";
 import { parseInput } from "../../middleware/validate-body";
-import { hosts } from "../../../jobs/export-chunks";
 
 export const txSchema: Schema = Joi.object({
   id: Joi.string()
@@ -62,7 +61,7 @@ export const handler: RequestHandler<{}, {}, Transaction> = async (
 
   req.log.info(`Redirection to node init`);
 
-  await broadcastTx(tx, hosts);
+  await broadcastTx(tx, process.env.AMPLIFY_GATEWAY_URL);
 
   req.log.info(`[new-tx]`, {
     ...tx,
