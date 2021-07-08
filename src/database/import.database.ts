55dcaac50c43dd4792a42b09472062e50c1c2c0a
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-07-08 18:07:01 +0200

    fixing pool client query

--- src/database/import.database.ts
+++ src/database/import.database.ts
@@ -1,20 +1,37 @@
-import fs from 'fs';
-import {config} from 'dotenv';
-import {indices} from '../utility/order.utility';
-import {connection} from '../database/connection.database';
-import {transactionFields} from '../database/transaction.database';
-import {from as copyFrom} from 'pg-copy-streams';
+import fs from "fs";
+import { PoolClient } from "pg";
+import { config } from "dotenv";
+import { indices } from "../utility/order.utility";
+import { pgConnection } from "../database/connection.database";
+import { transactionFields } from "../database/transaction.database";
+import { from as copyFrom } from "pg-copy-streams";
 
 config();
 
 export async function importBlocks(path: string) {
   return new Promise(async (resolve, reject) => {
+    let client: PoolClient;
     try {
-      const encoding = '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("height"))';
-      const stream = connection.query(`COPY blocks ("id", "previous_block", "mined_at", "height", "txs", "extended") FROM STDIN WITH ${encoding}`);
+      client = await pgConnection.connect();
+      const encoding =
+        "(FORMAT CSV, HEADER, ESCAPE '\\', DELIMITER '|', FORCE_NULL(\"height\"))";
+      const stream = client.query(
+        copyFrom(
+          `COPY blocks ("id", "previous_block", "mined_at", "height", "txs", "extended") FROM STDIN WITH ${encoding}`
+        )
+      );
       const fileStream = fs.createReadStream(path);
-      fileStream.on('error', reject);
-      fileStream.pipe(stream).on('finish', resolve).on('error', reject);
+      fileStream.on("error", reject);
+      fileStream
+        .pipe(stream)
+        .on("finish", () => {
+          client.release();
+          resolve(true);
+        })
+        .on("error", (err) => {
+          client.release();
+          reject(err);
+        });
     } catch (error) {
       return reject(error);
     }
@@ -23,17 +40,35 @@ export async function importBlocks(path: string) {
 
 export async function importTransactions(path: string) {
   return new Promise(async (resolve, reject) => {
+    let client: PoolClient;
     try {
+      client = await pgConnection.connect();
       const fields = transactionFields
-          .concat(indices)
-          .map((field) => `"${field}"`);
+        .concat(indices)
+        .map((field) => `"${field}"`);
 
-      const encoding = '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("format", "height", "data_size"))';
-      const stream = connection.query(`COPY transactions (${fields.join(',')}) FROM STDIN WITH ${encoding}`);
+      const encoding =
+        '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("format", "height", "data_size"))';
+      const stream = client.query(
+        copyFrom(
+          `COPY transactions (${fields.join(",")}) FROM STDIN WITH ${encoding}`
+        )
+      );
       const fileStream = fs.createReadStream(path);
-      fileStream.on('error', reject);
-      fileStream.pipe(stream).on('finish', resolve).on('error', reject);
-
+      fileStream.on("error", () => {
+        client.release();
+        reject();
+      });
+      fileStream
+        .pipe(stream)
+        .on("finish", () => {
+          client.release();
+          resolve(true);
+        })
+        .on("error", (err) => {
+          client.release();
+          reject(err);
+        });
     } catch (error) {
       return reject(error);
     }
@@ -42,13 +77,31 @@ export async function importTransactions(path: string) {
 
 export async function importTags(path: string) {
   return new Promise(async (resolve, reject) => {
+    let client: PoolClient;
     try {
-      const encoding = '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL(index))';
-      const stream = connection.query(`COPY tags ("tx_id", "index", "name", "value") FROM STDIN WITH ${encoding}`);
+      client = await pgConnection.connect();
+      const encoding =
+        "(FORMAT CSV, HEADER, ESCAPE '\\', DELIMITER '|', FORCE_NULL(index))";
+      const stream = client.query(
+        copyFrom(
+          `COPY tags ("tx_id", "index", "name", "value") FROM STDIN WITH ${encoding}`
+        )
+      );
       const fileStream = fs.createReadStream(path);
-      fileStream.on('error', reject);
-      fileStream.pipe(stream).on('finish', resolve).on('error', reject);
-
+      fileStream.on("error", () => {
+        client.release();
+        reject();
+      });
+      fileStream
+        .pipe(stream)
+        .on("finish", () => {
+          client.release();
+          resolve(true);
+        })
+        .on("error", (err) => {
+          client.release();
+          reject(err);
+        });
     } catch (error) {
       return reject(error);
     }
