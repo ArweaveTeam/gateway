6a417a8ace11ae40201bfce611319465c310da3f
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-07-08 17:37:44 +0200

    stream csv data into copy command to bypass superuser perm

--- src/database/import.database.ts
+++ src/database/import.database.ts
@@ -1,7 +1,9 @@
+import fs from 'fs';
 import {config} from 'dotenv';
 import {indices} from '../utility/order.utility';
 import {connection} from '../database/connection.database';
 import {transactionFields} from '../database/transaction.database';
+import {from as copyFrom} from 'pg-copy-streams';
 
 config();
 
@@ -9,9 +11,10 @@ export async function importBlocks(path: string) {
   return new Promise(async (resolve, reject) => {
     try {
       const encoding = '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("height"))';
-      await connection.raw(`COPY blocks ("id", "previous_block", "mined_at", "height", "txs", "extended") FROM '${path}' WITH ${encoding}`);
-
-      return resolve(true);
+      const stream = connection.query(`COPY blocks ("id", "previous_block", "mined_at", "height", "txs", "extended") FROM STDIN WITH ${encoding}`);
+      const fileStream = fs.createReadStream(path);
+      fileStream.on('error', reject);
+      fileStream.pipe(stream).on('finish', resolve).on('error', reject);
     } catch (error) {
       return reject(error);
     }
@@ -26,9 +29,11 @@ export async function importTransactions(path: string) {
           .map((field) => `"${field}"`);
 
       const encoding = '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("format", "height", "data_size"))';
-      await connection.raw(`COPY transactions (${fields.join(',')}) FROM '${path}' WITH ${encoding}`);
+      const stream = connection.query(`COPY transactions (${fields.join(',')}) FROM STDIN WITH ${encoding}`);
+      const fileStream = fs.createReadStream(path);
+      fileStream.on('error', reject);
+      fileStream.pipe(stream).on('finish', resolve).on('error', reject);
 
-      return resolve(true);
     } catch (error) {
       return reject(error);
     }
@@ -39,9 +44,11 @@ export async function importTags(path: string) {
   return new Promise(async (resolve, reject) => {
     try {
       const encoding = '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL(index))';
-      await connection.raw(`COPY tags ("tx_id", "index", "name", "value") FROM '${path}' WITH ${encoding}`);
+      const stream = connection.query(`COPY tags ("tx_id", "index", "name", "value") FROM STDIN WITH ${encoding}`);
+      const fileStream = fs.createReadStream(path);
+      fileStream.on('error', reject);
+      fileStream.pipe(stream).on('finish', resolve).on('error', reject);
 
-      return resolve(true);
     } catch (error) {
       return reject(error);
     }
