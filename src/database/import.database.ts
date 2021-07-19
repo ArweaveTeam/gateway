6c20c55b5603e7734461d931b7a454ecd5a2b727
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-07-19 15:19:17 +0200

    remove unneccecary async try-catches

--- src/database/import.database.ts
+++ src/database/import.database.ts
@@ -1,109 +1,94 @@
-import fs from "fs";
-import { PoolClient } from "pg";
-import { config } from "dotenv";
-import { indices } from "../utility/order.utility";
-import { pgConnection } from "../database/connection.database";
-import { transactionFields } from "../database/transaction.database";
-import { from as copyFrom } from "pg-copy-streams";
+import fs from 'fs';
+import { PoolClient } from 'pg';
+import { config } from 'dotenv';
+import { indices } from '../utility/order.utility';
+import { pgConnection } from '../database/connection.database';
+import { transactionFields } from '../database/transaction.database';
+import { from as copyFrom } from 'pg-copy-streams';
 
 config();
 
-export async function importBlocks(path: string) {
+export function importBlocks(path: string) {
   return new Promise(async (resolve, reject) => {
-    let client: PoolClient;
-    try {
-      client = await pgConnection.connect();
-      const encoding =
-        "(FORMAT CSV, HEADER, ESCAPE '\\', DELIMITER '|', FORCE_NULL(\"height\"))";
-      const stream = client.query(
-        copyFrom(
-          `COPY blocks ("id", "previous_block", "mined_at", "height", "txs", "extended") FROM STDIN WITH ${encoding}`
-        )
-      );
-      const fileStream = fs.createReadStream(path);
-      fileStream.on("error", reject);
-      fileStream
-        .pipe(stream)
-        .on("finish", () => {
-          client.release();
-          resolve(true);
-        })
-        .on("error", (err: unknown) => {
-          client.release();
-          reject(new String(err));
-        });
-    } catch (error) {
-      return reject(error);
-    }
+    const client: PoolClient = await pgConnection.connect();
+    const encoding =
+      "(FORMAT CSV, HEADER, ESCAPE '\\', DELIMITER '|', FORCE_NULL(\"height\"))";
+    const stream = client.query(
+      copyFrom(
+        `COPY blocks ("id", "previous_block", "mined_at", "height", "txs", "extended") FROM STDIN WITH ${encoding}`
+      )
+    );
+    const fileStream = fs.createReadStream(path);
+    fileStream.on('error', reject);
+    fileStream
+      .pipe(stream)
+      .on('finish', () => {
+        client.release();
+        resolve(true);
+      })
+      .on('error', (err: unknown) => {
+        client.release();
+        reject(new String(err));
+      });
   });
 }
 
-export async function importTransactions(path: string) {
+export function importTransactions(path: string) {
   return new Promise(async (resolve, reject) => {
-    let client: PoolClient;
-    try {
-      client = await pgConnection.connect();
-      const fields = transactionFields
-        .concat(indices)
-        .map((field) => `"${field}"`);
+    const client: PoolClient = await pgConnection.connect();
+    const fields = transactionFields
+      .concat(indices)
+      .map((field) => `"${field}"`);
 
-      const encoding =
-        '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("format", "height", "data_size"))';
-      const stream = client.query(
-        copyFrom(
-          `COPY transactions (${fields.join(",")}) FROM STDIN WITH ${encoding}`
-        )
-      );
-      const fileStream = fs.createReadStream(path);
-      fileStream.on("error", () => {
+    const encoding =
+      '(FORMAT CSV, HEADER, ESCAPE \'\\\', DELIMITER \'|\', FORCE_NULL("format", "height", "data_size"))';
+    const stream = client.query(
+      copyFrom(
+        `COPY transactions (${fields.join(',')}) FROM STDIN WITH ${encoding}`
+      )
+    );
+    const fileStream = fs.createReadStream(path);
+    fileStream.on('error', () => {
+      client.release();
+      reject();
+    });
+    fileStream
+      .pipe(stream)
+      .on('finish', () => {
         client.release();
-        reject();
+        resolve(true);
+      })
+      .on('error', (err: unknown) => {
+        client.release();
+        reject(new String(err));
       });
-      fileStream
-        .pipe(stream)
-        .on("finish", () => {
-          client.release();
-          resolve(true);
-        })
-        .on("error", (err: unknown) => {
-          client.release();
-          reject(new String(err));
-        });
-    } catch (error) {
-      return reject(error);
-    }
   });
 }
 
-export async function importTags(path: string) {
+export function importTags(path: string) {
   return new Promise(async (resolve, reject) => {
-    let client: PoolClient;
-    try {
-      client = await pgConnection.connect();
-      const encoding =
-        "(FORMAT CSV, HEADER, ESCAPE '\\', DELIMITER '|', FORCE_NULL(index))";
-      const stream = client.query(
-        copyFrom(
-          `COPY tags ("tx_id", "index", "name", "value") FROM STDIN WITH ${encoding}`
-        )
-      );
-      const fileStream = fs.createReadStream(path);
-      fileStream.on("error", () => {
+    const client: PoolClient = await pgConnection.connect();
+    const encoding =
+      "(FORMAT CSV, HEADER, ESCAPE '\\', DELIMITER '|', FORCE_NULL(index))";
+    const stream = client.query(
+      copyFrom(
+        `COPY tags ("tx_id", "index", "name", "value") FROM STDIN WITH ${encoding}`
+      )
+    );
+    const fileStream = fs.createReadStream(path);
+    fileStream.on('error', () => {
+      client.release();
+      reject();
+    });
+    fileStream
+      .pipe(stream)
+      .on('finish', () => {
         client.release();
-        reject();
+        resolve(true);
+      })
+      .on('error', (err: unknown) => {
+        client.release();
+        reject(new String(err));
       });
-      fileStream
-        .pipe(stream)
-        .on("finish", () => {
-          client.release();
-          resolve(true);
-        })
-        .on("error", (err: unknown) => {
-          client.release();
-          reject(new String(err));
-        });
-    } catch (error) {
-      return reject(error);
-    }
   });
 }
--- src/database/sync.database.ts
+++ src/database/sync.database.ts
@@ -1,21 +1,34 @@
 import ProgressBar from 'progress';
-import {DataItemJson} from 'arweave-bundles';
-import {config} from 'dotenv';
-import {getLastBlock} from '../utility/height.utility';
-import {serializeBlock, serializeTransaction, serializeAnsTransaction, serializeTags} from '../utility/serialize.utility';
-import {streams, initStreams, resetCacheStreams} from '../utility/csv.utility';
-import {log} from '../utility/log.utility';
-import {ansBundles} from '../utility/ans.utility';
-import {mkdir} from '../utility/file.utility';
-import {sleep} from '../utility/sleep.utility';
-import {TestSuite} from '../utility/mocha.utility';
-import {getNodeInfo} from '../query/node.query';
-import {block} from '../query/block.query';
-import {transaction, tagValue, Tag} from '../query/transaction.query';
-import {getDataFromChunks} from '../query/node.query';
-import {importBlocks, importTransactions, importTags} from './import.database';
-import {DatabaseTag} from './transaction.database';
-import {cacheANSEntries} from '../caching/ans.entry.caching';
+import { DataItemJson } from 'arweave-bundles';
+import { config } from 'dotenv';
+import { getLastBlock } from '../utility/height.utility';
+import {
+  serializeBlock,
+  serializeTransaction,
+  serializeAnsTransaction,
+  serializeTags,
+} from '../utility/serialize.utility';
+import {
+  streams,
+  initStreams,
+  resetCacheStreams,
+} from '../utility/csv.utility';
+import { log } from '../utility/log.utility';
+import { ansBundles } from '../utility/ans.utility';
+import { mkdir } from '../utility/file.utility';
+import { sleep } from '../utility/sleep.utility';
+import { TestSuite } from '../utility/mocha.utility';
+import { getNodeInfo } from '../query/node.query';
+import { block } from '../query/block.query';
+import { transaction, tagValue, Tag } from '../query/transaction.query';
+import { getDataFromChunks } from '../query/node.query';
+import {
+  importBlocks,
+  importTransactions,
+  importTags,
+} from './import.database';
+import { DatabaseTag } from './transaction.database';
+import { cacheANSEntries } from '../caching/ans.entry.caching';
 
 config();
 mkdir('snapshot');
@@ -32,14 +45,11 @@ export let currentHeight = 0;
 export let timer = setTimeout(() => {}, 0);
 
 export function configureSyncBar(start: number, end: number) {
-  bar = new ProgressBar(
-      ':current/:total blocks synced [:bar] :percent :etas',
-      {
-        complete: '|',
-        incomplete: ' ',
-        total: end - start,
-      },
-  );
+  bar = new ProgressBar(':current/:total blocks synced [:bar] :percent :etas', {
+    complete: '|',
+    incomplete: ' ',
+    total: end - start,
+  });
 }
 
 export async function startSync() {
@@ -47,7 +57,9 @@ export async function startSync() {
   currentHeight = startHeight;
 
   if (parallelization > 0) {
-    log.info(`[database] starting sync, parallelization is set to ${parallelization}`);
+    log.info(
+      `[database] starting sync, parallelization is set to ${parallelization}`
+    );
     if (storeSnapshot) {
       log.info('[snapshot] also writing new blocks to the snapshot folder');
     }
@@ -60,7 +72,9 @@ export async function startSync() {
       configureSyncBar(startHeight, nodeInfo.height);
       topHeight = nodeInfo.height;
 
-      log.info(`[database] database is currently at height ${startHeight}, resuming sync to ${topHeight}`);
+      log.info(
+        `[database] database is currently at height ${startHeight}, resuming sync to ${topHeight}`
+      );
 
       bar.tick();
       await parallelize(startHeight + 1);
@@ -89,7 +103,9 @@ export async function parallelize(height: number) {
     await sleep(30000);
     const nodeInfo = await getNodeInfo();
     if (nodeInfo.height > topHeight) {
-      log.info(`[database] updated height from ${topHeight} to ${nodeInfo.height} syncing new blocks`);
+      log.info(
+        `[database] updated height from ${topHeight} to ${nodeInfo.height} syncing new blocks`
+      );
       topHeight = nodeInfo.height;
     }
 
@@ -108,24 +124,29 @@ export async function parallelize(height: number) {
     try {
       await importBlocks(`${process.cwd()}/cache/block.csv`);
     } catch (error) {
-      log.error('[sync] importing new blocks failed most likely due to it already being in the DB');
+      log.error(
+        '[sync] importing new blocks failed most likely due to it already being in the DB'
+      );
       log.error(error);
     }
 
     try {
       await importTransactions(`${process.cwd()}/cache/transaction.csv`);
     } catch (error) {
-      log.error('[sync] importing new transactions failed most likely due to it already being in the DB');
+      log.error(
+        '[sync] importing new transactions failed most likely due to it already being in the DB'
+      );
       log.error(error);
     }
 
     try {
       await importTags(`${process.cwd()}/cache/tags.csv`);
     } catch (error) {
-      log.error('[sync] importing new tags failed most likely due to it already being in the DB');
+      log.error(
+        '[sync] importing new tags failed most likely due to it already being in the DB'
+      );
       log.error(error);
     }
-
 
     resetCacheStreams();
 
@@ -144,7 +165,7 @@ export async function parallelize(height: number) {
 export async function storeBlock(height: number, retry: number = 0) {
   try {
     const currentBlock = await block(height);
-    const {formattedBlock, input} = serializeBlock(currentBlock, height);
+    const { formattedBlock, input } = serializeBlock(currentBlock, height);
 
     streams.block.cache.write(input);
 
@@ -153,15 +174,22 @@ export async function storeBlock(height: number, retry: number = 0) {
     }
 
     if (height > 0) {
-      await storeTransactions(JSON.parse(formattedBlock.txs) as Array<string>, height);
+      await storeTransactions(
+        JSON.parse(formattedBlock.txs) as Array<string>,
+        height
+      );
     }
   } catch (error) {
     if (SIGKILL === false) {
       if (retry >= 25) {
-        log.info(`[snapshot] there were problems retrieving ${height}, restarting the server`);
+        log.info(
+          `[snapshot] there were problems retrieving ${height}, restarting the server`
+        );
         await startSync();
       } else {
-        log.info(`[snapshot] could not retrieve block at height ${height}, retrying`);
+        log.info(
+          `[snapshot] could not retrieve block at height ${height}, retrying`
+        );
         await storeBlock(height, retry + 1);
       }
     }
@@ -179,10 +207,17 @@ export async function storeTransactions(txs: Array<string>, height: number) {
   await Promise.all(batch);
 }
 
-export async function storeTransaction(tx: string, height: number, retry: boolean = true) {
+export async function storeTransaction(
+  tx: string,
+  height: number,
+  retry: boolean = true
+) {
   try {
     const currentTransaction = await transaction(tx);
-    const {formattedTransaction, preservedTags, input} = serializeTransaction(currentTransaction, height);
+    const { formattedTransaction, preservedTags, input } = serializeTransaction(
+      currentTransaction,
+      height
+    );
 
     streams.transaction.cache.write(input);
 
@@ -199,7 +234,13 @@ export async function storeTransaction(tx: string, height: number, retry: boolea
     }
   } catch (error) {
     console.log('');
-    log.info(`[database] could not retrieve tx ${tx} at height ${height} ${retry ? ', attempting to retrieve again' : ', missing tx stored in .rescan'}`);
+    log.info(
+      `[database] could not retrieve tx ${tx} at height ${height} ${
+        retry
+          ? ', attempting to retrieve again'
+          : ', missing tx stored in .rescan'
+      }`
+    );
     if (retry) {
       await storeTransaction(tx, height, false);
     } else {
@@ -211,7 +252,11 @@ export async function storeTransaction(tx: string, height: number, retry: boolea
   }
 }
 
-export async function processAns(id: string, height: number, retry: boolean = true) {
+export async function processAns(
+  id: string,
+  height: number,
+  retry: boolean = true
+) {
   try {
     const ansPayload = await getDataFromChunks(id);
     const ansTxs = await ansBundles.unbundleData(ansPayload.toString('utf-8'));
@@ -222,7 +267,9 @@ export async function processAns(id: string, height: number, retry: boolean = tr
     if (retry) {
       await processAns(id, height, false);
     } else {
-      log.info(`[database] malformed ANS payload at height ${height} for tx ${id}`);
+      log.info(
+        `[database] malformed ANS payload at height ${height} for tx ${id}`
+      );
       streams.rescan.cache.write(`${id}|${height}|ans\n`);
       if (storeSnapshot) {
         streams.rescan.snapshot.write(`${id}|${height}|ans\n`);
@@ -231,10 +278,13 @@ export async function processAns(id: string, height: number, retry: boolean = tr
   }
 }
 
-export async function processANSTransaction(ansTxs: Array<DataItemJson>, height: number) {
+export async function processANSTransaction(
+  ansTxs: Array<DataItemJson>,
+  height: number
+) {
   for (let i = 0; i < ansTxs.length; i++) {
     const ansTx = ansTxs[i];
-    const {ansTags, input} = serializeAnsTransaction(ansTx, height);
+    const { ansTags, input } = serializeAnsTransaction(ansTx, height);
 
     streams.transaction.cache.write(input);
 
@@ -244,7 +294,7 @@ export async function processANSTransaction(ansTxs: Array<DataItemJson>, height:
 
     for (let ii = 0; ii < ansTags.length; ii++) {
       const ansTag = ansTags[ii];
-      const {name, value} = ansTag;
+      const { name, value } = ansTag;
 
       const tag: DatabaseTag = {
         tx_id: ansTx.id,
@@ -267,7 +317,7 @@ export async function processANSTransaction(ansTxs: Array<DataItemJson>, height:
 export function storeTags(tx_id: string, tags: Array<Tag>) {
   for (let i = 0; i < tags.length; i++) {
     const tag = tags[i];
-    const {input} = serializeTags(tx_id, i, tag);
+    const { input } = serializeTags(tx_id, i, tag);
     streams.tags.cache.write(input);
     if (storeSnapshot) {
       streams.tags.snapshot.write(input);
@@ -275,11 +325,12 @@ export function storeTags(tx_id: string, tags: Array<Tag>) {
   }
 }
 
-
 export function signalHook() {
   if (!TestSuite) {
     process.on('SIGINT', () => {
-      log.info('[database] ensuring all blocks are stored before exit, you may see some extra output in console');
+      log.info(
+        '[database] ensuring all blocks are stored before exit, you may see some extra output in console'
+      );
       SIGKILL = true;
       setInterval(() => {
         if (SIGINT === false) {
